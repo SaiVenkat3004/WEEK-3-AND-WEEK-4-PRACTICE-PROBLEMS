142340a3cb634d81cdfd896e6463f4ffa6a55a46
SaiVenkat3004 <sy1704@srmist.edu.in>
2026-03-09 09:25:35 +0530

    Added Level3 Problem5 Armstrong

---
+++ Problem5.java
@@ -0,0 +1,23 @@
+// Problem5 - Armstrong Number
+import java.util.Scanner;
+
+public class Problem5 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int number = sc.nextInt();
+        int originalNumber = number;
+        int sum = 0;
+
+        while(originalNumber != 0) {
+            int digit = originalNumber % 10;
+            sum = sum + digit * digit * digit;
+            originalNumber = originalNumber / 10;
+        }
+
+        if(sum == number)
+            System.out.println("Armstrong Number");
+        else
+            System.out.println("Not Armstrong Number");
+    }
+}
